5761ce42e4e6e99c56bf84ce1ea3071bad7a7b39
Tim Monks <t.r.monks@gmail.com>
2013-11-29 09:03:39 +0000

    basic reactivity

--- main.hs
+++ main.hs
@@ -2,44 +2,36 @@
 import Reactive.Banana
 import Reactive.Banana.Frameworks
 import UI.HSCurses.Curses
-import System.Time (getClockTime)
+import Control.Applicative
+
+--castEnum = toEnum . fromEnum
+
+posMove (dx, dy) = \(x, y) -> (x + dx, y + dy)
+
+charToMove c = case c of
+    'j' -> (1, 0)
+    _   -> (0, 0)
+
+keyToMove k = posMove $ case k of
+    (KeyChar c) -> charToMove c
+    _           -> (0, 0)
 
 main :: IO ()
 main = do
-   -- initialize your GUI framework
-   -- window <- newWindow
-   -- ...
-   (ah, ahf) <- newAddHandler
+    --initScr
+    --echo False
 
-   -- describe the event network
-   let networkDescription :: forall t. Frameworks t => Moment t ()
-       networkDescription = do
-           -- input: obtain  Event  from functions that register event
-           -- handlers
-           --emouse    <- fromAddHandler $ registerMouseEvent window
-           --ekeyboard <- fromAddHandler $ registerKeyEvent window
-           -- input: obtain  Behavior  from changes
-           --btext     <- fromChanges    "" $ registerTextChange editBox
-           -- input: obtain  Behavior  from mutable data by polling
-           --bdie      <- fromPoll       $ randomRIO (1,6)
-           -- only updated when there is input
-           eTick <- fromAddHandler ah
-           bTime <- fromPoll getClockTime
-           eTime <- changes bTime
+    (getChHandler, getChCallback) <- newAddHandler
 
-           -- express event graph
-           let
-               -- behavior1 = accumB ...
-               -- ...
-               -- event15 = union event13 event14
+    let networkDescription :: forall t. Frameworks t => Moment t ()
+        networkDescription = do
+            eKey  <- fromAddHandler getChHandler
+            let eMove = keyToMove <$> eKey
+            let bPos  = accumB (0, 0) eMove
+            ePos  <- changes bPos
+            reactimate (print <$> ePos)
 
-           -- output: animate some event occurences
-           reactimate $ fmap print eTime
-           -- reactimate $ fmap drawCircle eventCircle
-
-   -- compile network description into a network
-   network <- compile networkDescription
-   -- register handlers and start producing outputs
-   putStrLn "compiled"
-   actuate network
-   putStrLn "actuated"
+    network <- compile networkDescription
+    actuate network
+    getChCallback $ KeyChar 'j'
+    getChCallback $ KeyChar 'j'
